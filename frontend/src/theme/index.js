ec3250767cb96114675f31ab0d29755197d0f371
Shahtr1 <tramboos123@gmail.com>
2025-02-21 19:50:49 +0530

    changed dark mode color

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -7,6 +7,7 @@ import selectTheme from "@/theme/selectTheme.js";
 import radioTheme from "@/theme/radioTheme.js";
 import checkboxTheme from "@/theme/checkboxTheme.js";
 import textTheme from "@/theme/textTheme.js";
+import colors from "@/theme/colors.js";
 
 const config = {
   initialColorMode: "light",
@@ -17,28 +18,10 @@ const styles = {
   global: (props) => ({
     body: {
       color: mode("black", "whiteAlpha.900")(props),
-      bg: mode("white", "#101218")(props),
+      bg: mode("white", "gray.900")(props),
+      fontFamily: "'Nunito Sans', sans-serif",
     },
   }),
-};
-
-const colors = {
-  brand: {
-    50: "#e3fdfc",
-    100: "#c1f7f4",
-    200: "#97ede6",
-    300: "#6ae2d9",
-    400: "#44d7cc",
-    500: "#27A69F",
-    600: "#1e857f",
-    700: "#166561",
-    800: "#0e4443",
-    900: "#072423",
-  },
-  text: {
-    primary: "#444648",
-    secondary: "#90949C",
-  },
 };
 
 const components = {
